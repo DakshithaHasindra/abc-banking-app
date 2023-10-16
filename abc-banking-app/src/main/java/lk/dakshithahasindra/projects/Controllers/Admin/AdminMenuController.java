d314883d691a9c2ab7382beef3afdb2144a62348
Dakshitha Hasindra <hasindradh@gmail.com>
2023-10-16 23:28:10 +0530

    "Complete Admin Menu GUI"

---
+++ abc-banking-app/src/main/java/lk/dakshithahasindra/projects/Controllers/Admin/AdminMenuController.java
@@ -0,0 +1,10 @@
+package lk.dakshithahasindra.projects.Controllers.Admin;
+
+import javafx.scene.control.Button;
+
+public class AdminMenuController {
+    public Button btnCreateClient;
+    public Button btnClients;
+    public Button btnDeposite;
+    public Button btnLogOut;
+}
