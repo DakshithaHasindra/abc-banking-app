63d2f31c471935d828871e1a46adae219814df4c
Dakshitha Hasindra <hasindradh@gmail.com>
2023-10-20 18:34:59 +0530

    clients view in admin completed

---
+++ abc-banking-app/src/main/java/lk/dakshithahasindra/projects/Controllers/Admin/ClientsController.java
@@ -0,0 +1,13 @@
+package lk.dakshithahasindra.projects.Controllers.Admin;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientsController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
