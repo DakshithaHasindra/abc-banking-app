b9512140c6a0c69e80b094843c45b3857d27385f
Dakshitha Hasindra <hasindradh@gmail.com>
2023-10-21 11:14:45 +0530

    Completed Schema.sql and database

--- abc-banking-app/src/main/java/lk/dakshithahasindra/projects/AppInitializer.java
+++ abc-banking-app/src/main/java/lk/dakshithahasindra/projects/AppInitializer.java
@@ -1,19 +1,30 @@
 package lk.dakshithahasindra.projects;
 
+import com.mysql.cj.conf.url.SingleConnectionUrl;
 import javafx.application.Application;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import lk.dakshithahasindra.projects.Models.Model;
+import lk.dakshithahasindra.projects.Models.SingleDataConnection;
 import lk.dakshithahasindra.projects.Views.ViewFactory;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class AppInitializer extends Application {
 
     public static void main(String[] args) {
-        launch(args);
+        try(Connection connection = SingleDataConnection.getInstance().getConnection()){
+            launch(args);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
--- abc-banking-app/src/main/resources/database/database.sql
+++ abc-banking-app/src/main/resources/database/database.sql
@@ -1,13 +1,13 @@
 CREATE TABLE clients (
     id VARCHAR(10) PRIMARY KEY ,
     f_name VARCHAR(50) NOT NULL ,
-    l_name VARCHAR(50) NOT NULL ,
+    l_name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE account (
   account_number VARCHAR(20) PRIMARY KEY ,
   is_saving_acc BOOLEAN NOT NULL ,
-  balance DECIMAL(8,2) DEFAULT 0.00,
+  balance DECIMAL(8,2) ,
   client_id VARCHAR(10) NOT NULL ,
   CONSTRAINT fk_account FOREIGN KEY (client_id) REFERENCES clients(id)
 );
@@ -16,7 +16,7 @@ CREATE TABLE transactions(
     trans_id VARCHAR(10) PRIMARY KEY ,
     amount DECIMAL(8,2) NOT NULL ,
     sender_acc_no VARCHAR(20) NOT NULL ,
-    receiver_acc_no VARCHAR(20) NOT NULL ,
+    receiver_acc_no VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE accounts_transactions(
