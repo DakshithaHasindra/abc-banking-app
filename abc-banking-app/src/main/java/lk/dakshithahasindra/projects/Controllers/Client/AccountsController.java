24186ed97e3a8814797ea57dacc24a8df32946c5
Dakshitha Hasindra <hasindradh@gmail.com>
2023-11-14 23:23:33 +0530

    "Transfer own Savings Account Completed"

--- abc-banking-app/src/main/java/lk/dakshithahasindra/projects/Controllers/Client/AccountsController.java
+++ abc-banking-app/src/main/java/lk/dakshithahasindra/projects/Controllers/Client/AccountsController.java
@@ -1,14 +1,23 @@
 package lk.dakshithahasindra.projects.Controllers.Client;
 
 import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import lk.dakshithahasindra.projects.Models.Client;
+import lk.dakshithahasindra.projects.Models.DB.TransactionsDataSource;
+import lk.dakshithahasindra.projects.Models.SingleDataConnection;
+import lk.dakshithahasindra.projects.Models.Transaction;
 import lk.dakshithahasindra.projects.Views.sharedData.SharedCurrentLoginData;
 
 import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class AccountsController {
     public AnchorPane rootClientProfile;
@@ -24,33 +33,85 @@ public class AccountsController {
     public Button btnTransferToSA;
     public Button btnTransferToCA;
     public TextField txtTransferAmountToCA;
+    public Text lblCheckingAcc;
+    public VBox VBoxChekingAcc;
+    public Text lblSavingsAcc;
+    public VBox VBoxSavingsAcc;
 
     public void initialize(){
 //        System.out.println(SharedCurrentLoginData.getInstance().loginID);
         loadAccountData();
+        Client loggedInClient = SharedCurrentLoginData.loggedInClient;
+        if(loggedInClient.checkingAccountProperty().get()==null){
+            for (Node node:
+                    new Node[]{txtTransferAmountToCA,txtTransferAmountToSA,btnTransferToCA,btnTransferToSA}) {
+                node.setDisable(true);
+            }
+            lblCheckingAcc.setVisible(false);
+            VBoxChekingAcc.setVisible(false);
+        }
 
+        if(loggedInClient.savingsAccountProperty().get()==null){
+            for (Node node:
+                    new Node[]{txtTransferAmountToCA,txtTransferAmountToSA,btnTransferToCA,btnTransferToSA}) {
+                node.setDisable(true);
+            }
+            lblSavingsAcc.setVisible(false);
+            VBoxSavingsAcc.setVisible(false);
+        }
     }
 
     private void loadAccountData() {
         Client loggedClient = SharedCurrentLoginData.loggedInClient;
-        lblCAAccountNumber.setText(loggedClient.checkingAccountProperty().get().accountNumberProperty().get());
-        lblCATransactionLimit.setText("Rs. 100,000.00");
+        if (loggedClient.checkingAccountProperty().get()!=null){
+            lblCAAccountNumber.setText(loggedClient.checkingAccountProperty().get().accountNumberProperty().get());
+            lblCATransactionLimit.setText("Rs. 100,000.00");
 //        lblCACreatedDate.setText(); TODO: SetCreatedDate
-        lblCABalance.setText("Rs. "+loggedClient.checkingAccountProperty().get().balanceProperty().get());
+            lblCABalance.setText("Rs. "+loggedClient.checkingAccountProperty().get().balanceProperty().get());
+        }
 
-        lblSANumber.setText(loggedClient.savingsAccountProperty().get().accountNumberProperty().get());
-        lblSAWithdrawalLimit.setText("Rs. 100,000.00");
+        if (loggedClient.savingsAccountProperty().get()!=null){
+            lblSANumber.setText(loggedClient.savingsAccountProperty().get().accountNumberProperty().get());
+            lblSAWithdrawalLimit.setText("Rs. 100,000.00");
 //        lblSACreatedDate.setText(); TODO: SetCreatedDate
-        lblSABalance.setText("Rs. "+loggedClient.savingsAccountProperty().get().balanceProperty().get());
+            lblSABalance.setText("Rs. "+loggedClient.savingsAccountProperty().get().balanceProperty().get());
+        }
     }
 
     public void btnTransferToSAOnAction(ActionEvent actionEvent) {
+        Client loggedClient = SharedCurrentLoginData.loggedInClient;
         if(!isValidAmount(txtTransferAmountToSA.getText().strip())){
             txtTransferAmountToSA.selectAll();
+            txtTransferAmountToSA.requestFocus();
             return;
         }
-//        BigDecimal transferAmount = new BigDecimal(txtTransferAmountToSA.getText());
-//        System.out.println(transferAmount);
+        double transferAmount = Double.parseDouble(txtTransferAmountToSA.getText());
+        if(transferAmount>loggedClient.checkingAccountProperty().get().balanceProperty().get()-1000){
+            new Alert(Alert.AlertType.ERROR,"Insufficient Balance").show();
+            return;
+        }
+        Connection connection = SingleDataConnection.getInstance().getConnection();
+
+        try {
+            connection.setAutoCommit(false);
+            TransactionsDataSource.updateAccountBalance(-transferAmount,loggedClient.checkingAccountProperty().get().accountNumberProperty().get());
+            TransactionsDataSource.updateAccountBalance(transferAmount,loggedClient.savingsAccountProperty().get().accountNumberProperty().get());
+
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+               e.printStackTrace();
+            }
+            new Alert(Alert.AlertType.ERROR,"Unknown Error occurred").show();
+            e.printStackTrace();
+        }
+        try {
+            connection.setAutoCommit(true);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
--- abc-banking-app/src/main/java/lk/dakshithahasindra/projects/Models/DB/ClientDataSource.java
+++ abc-banking-app/src/main/java/lk/dakshithahasindra/projects/Models/DB/ClientDataSource.java
@@ -7,22 +7,22 @@ import java.util.ArrayList;
 
 public class ClientDataSource {
     private static Connection connection = SingleDataConnection.getInstance().getConnection();
-    public static int nextCustomerId() throws SQLException {
-        Statement statement1 = connection.createStatement();
-        statement1.executeUpdate("INSERT INTO clients (f_name, l_name) VALUES ('dhgfch','kjbdk')");
-
-        Statement statement = connection.createStatement();
-//        ResultSet rst = statement.executeQuery("SELECT `auto_increment` FROM INFORMATION_SCHEMA.TABLES\n" +
-//                "WHERE table_name = 'clients'");
-
-        ResultSet rst = statement.executeQuery("SELECT AUTO_INCREMENT\n" +
-                "FROM information_schema.tables\n" +
-                "WHERE table_name = 'clients'\n" +
-                "AND table_schema = DATABASE( )");
-        rst.next();
-        int lastID = rst.getInt("AUTO_INCREMENT");
-        return lastID;
-    }
+//    public static int nextCustomerId() throws SQLException {
+//        Statement statement1 = connection.createStatement();
+//        statement1.executeUpdate("INSERT INTO clients (f_name, l_name) VALUES ('dhgfch','kjbdk')");
+//
+//        Statement statement = connection.createStatement();
+////        ResultSet rst = statement.executeQuery("SELECT `auto_increment` FROM INFORMATION_SCHEMA.TABLES\n" +
+////                "WHERE table_name = 'clients'");
+//
+//        ResultSet rst = statement.executeQuery("SELECT AUTO_INCREMENT\n" +
+//                "FROM information_schema.tables\n" +
+//                "WHERE table_name = 'clients'\n" +
+//                "AND table_schema = DATABASE( )");
+//        rst.next();
+//        int lastID = rst.getInt("AUTO_INCREMENT");
+//        return lastID;
+//    }
 
     public static int insertCustomer(String fname, String lname,String password) throws SQLException {
         PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO clients(f_name, l_name,password) VALUES (?,?,?)");
